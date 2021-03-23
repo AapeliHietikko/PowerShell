a8384ee92aa9afa8130457481f1f043457d3573d
AapeliH <33112891+AapeliH@users.noreply.github.com>
2021-03-23 09:04:10 +0200

    Update Find-InactiveLocalUserProfiles.ps1
    udpate on logic and add exclusion

--- teemu/Find-InactiveLocalUserProfiles.ps1
+++ teemu/Find-InactiveLocalUserProfiles.ps1
@@ -1,8 +1,15 @@
+$skipUsers = "järjestelmänvalvoja","toinenkayttaja"
+
+
 $activeUsers = Get-EventLog System -Source Microsoft-Windows-WinLogon -After (Get-Date).AddDays(-30) |
     select @{N='User';E={(New-Object System.Security.Principal.SecurityIdentifier $_.ReplacementStrings[1]).Translate([System.Security.Principal.NTAccount])}},TimeGenerated |
         Group-Object user | select name,@{N='time';E={$_.group[0].TimeGenerated}}
 
-$localUserPaths = get-wmiobject win32_userprofile -Filter "special='False'" | select LocalPath,@{N='BaseName';E={$_.LocalPath.split('\')[-1]}}
+
+$localUserPaths = Get-WmiObject -class win32_userprofile -Filter "special='False'" | 
+    select *,@{N='BaseName';E={$_.LocalPath.split('\')[-1]}},@{N='wmiFilterPath';E={$_.LocalPath.replace('\','\\')}} | 
+        where {$_.BaseName -notin $skipUsers}
+
 
 $activeSamids = $activeUsers.name | foreach {
     $_.split('\')[-1]
@@ -11,8 +18,7 @@ $localUserPaths | foreach {
 
     if (-not ($_.basename -in $activeSamids)) {
 
-        $_
+        Get-WmiObject -class win32_userprofile -Filter "LocalPath='$($_.wmiFilterPath)'" | Remove-WmiObject -WhatIf #-confirm:0 -force
 
         }
 }
-
