0ac85841a5f9e7dd3455647a55338957af0376a0
AapeliH <33112891+AapeliH@users.noreply.github.com>
2021-05-20 10:56:44 +0300

    Create demo_try_catch.ps1

---
+++ teemu/demo_try_catch.ps1
@@ -0,0 +1,19 @@
+try 
+{
+
+    remove-item 'HKCU:\Control Panel\PowerCfg\PowerPolicies\5' -ErrorAction Stop
+}
+catch
+{
+    "remove item pCFG 5 failed" | Out-File c:\temp\asentaja\logi,txt
+}
+
+try 
+{
+
+    remove-item 'HKCU:\Control Panel\PowerCfg\PowerPolicies\4' -ErrorAction Stop
+}
+catch
+{
+    "remove item pCFG 4 failed" | Out-File c:\temp\asentaja\logi,txt
+}
