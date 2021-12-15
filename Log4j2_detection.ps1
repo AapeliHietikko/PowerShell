b27ae99d4dbc28837a912b123ef4833540c0de26
AapeliH <33112891+AapeliH@users.noreply.github.com>
2021-12-15 15:28:13 +0200

    Update Log4j2_detection.ps1

--- Log4j2_detection.ps1
+++ Log4j2_detection.ps1
@@ -1,9 +1,3 @@
-    <#
-    based on this one, I think. Got the original code through teams chat
-    https://github.com/sp4ir/incidentresponse/blob/main/Get-Log4shellVuln.ps1
-    #>
-    
-    
     Add-Type -AssemblyName System.IO.Compression
     Add-Type -AssemblyName System.IO.Compression.FileSystem
     
@@ -12,9 +6,10 @@
     $log4Filter         = "log4j*.jar"
     $targetManifestFile = "$logFolder\log4j-manifest.txt"
     
-    $jarFiles   = Get-PSDrive | Where-Object { $_.Name.length -eq 1 } | Select-Object -ExpandProperty Root | Get-ChildItem -File -Recurse -Filter $log4Filter -ea 0 | foreach {select-string "JndiLookup.class" $_} | select -ExpandProperty Path | Group-Object | select -expand name
-    
-    $output = foreach ($jarFile in $jarFiles)
+    $jarFiles   = Get-PSDrive | Where-Object { $_.Name.length -eq 1 } | Select-Object -ExpandProperty Root | Get-ChildItem -File -Recurse -Filter $log4Filter -ea 0
+    $Log4JFiles = $jarFiles | foreach {select-string "JndiLookup.class" $_} | select -ExpandProperty Path | Group-Object | select -expand name
+        
+    $output = foreach ($jarFile in $Log4JFiles)
     {
         $zip = [System.IO.Compression.ZipFile]::OpenRead($jarFile)
     
@@ -26,12 +21,15 @@
             Remove-Item $targetManifestFile -ErrorAction SilentlyContinue
     
             $implementationVersion_ = [version]$implementationVersion.Replace('Implementation-Version: ', '')
+
+            [PSCustomObject][ordered]@{
+                'FilePath'   = $jarFile
+                #'Version'    = $implementationVersion_
+                #'ModifyDate' = $modifyDate 
+                'JndiLookup' = $true
+                }
+            
             if ($implementationVersion_ -lt '2.16.0') {
-                [PSCustomObject][ordered]@{
-                    'FilePath'   = $jarFile
-                    'Version'    = $implementationVersion_
-                    'ModifyDate' = $modifyDate 
-                    }
                  $vulnerable = 1
             } #if implementation version
     
@@ -47,9 +45,13 @@
         {
         "$($java.count) instance(s) of java running"
         }
-        
-        $output
+
     } else
     {
     "no log4j vulnerability found"
-    }
+    }    
+
+$output
+#$jarFiles | select @{N='FilePath';e={$_.fullName}}, @{N='Version';e={""}}, @{N='ModifyDate';e={$_.LastWriteTime}}, @{N='JndiLookup';e={$false}}
+ $jarFiles | select @{N='FilePath';e={$_.fullName}}, @{N='JndiLookup';e={$false}}
+    
